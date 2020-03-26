fed47a1d0b7ec9adbcbb09172d385764a2192b7f
Kjell Skogsrud <kjell@skogsrud.net>
2020-03-26 17:06:01 +0100

    Cleaned up the code and updated the README with some more info

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ It is made to be used on windows as a right-click option on folders. So simply r
 * Add that repository as the origin remote
 * Push to master to origin 
 
+### It doesn't work!
+It does, but there are some things it assumes you have.
+* It uses SSH for authentication with the remote, and for that to work in this way it assumes you have an SSH agent and have setup your github account with SSH keys.
+* It want's a username and personal token from github to use the API. A personal token is not the same as your password. [You get tokens her](https://github.com/settings/tokens)
+
 ## Installation?
 The file is made to be used as a windows right-click option on directories. A .reg files is included that can be used to add the option.
 Remember to change the paths in the reg file to where your files are located. The two paths you need are the location of python.exe and the location of git-init.py(from this repo)
--- git-init.py
+++ git-init.py
@@ -3,14 +3,11 @@ import os
 import sys
 
 # CHANGE THIS
-import loginDetails # This is where I keep you userName and token, just remove this line
-userName = loginDetails.username # replace getUsername() with "yourusername"
-token = loginDetails.token # replace getToken() with "yourtoken"
+import loginDetails # This is where I keep the userName and token, just remove this line
+userName = loginDetails.username # replace loginDetails.username with "yourusername"
+token = loginDetails.token # replace loginDetails.token with "yourtoken"
 # to get a token go here, and make a personal access token: https://github.com/settings/tokens
 # CHANGE THIS
-
-
-defaultgitIgnoreFile = open(r'{path}'.format(path=sys.argv[0].replace("git-init.py","defaultGitIgnore")),'r')
 
 calledPath = sys.argv[1]
 
@@ -21,29 +18,33 @@ if os.path.exists(r'{path}\.git'.format(path=calledPath)) is not False:
 # Not a .git folder, then lets git init
 os.system("git init {path}".format(path=calledPath))
 
-# Is there a README.md file in that folder that we can open?
-# If there is, we open it, and if not it is created.
-readme = open(r'{path}\README.md'.format(path=calledPath), 'w+')
-readme.close()
-
 os.chdir(calledPath) # enter the folder of the repo now to do some git work
 os.system("git commit -m empty --allow-empty") # make the first empty commit
 
+# Is there a README.md file in that folder that we can open?
+# If there is, we open it, and if not it is created.
+readme = open(r'{path}\README.md'.format(path=calledPath), 'a+')
+readme.close()
+
 # Make the .gitignore file based on the default file
-gitIgnore = open(r'{path}\.gitignore'.format(path=calledPath),'w+')
-gitIgnore.write(defaultgitIgnoreFile.read())
-gitIgnore.close()
+defaultgitIgnoreFile = open(r'{path}'.format(path=sys.argv[0].replace("git-init.py","defaultGitIgnore")),'r') # open the default file
+gitIgnore = open(r'{path}\.gitignore'.format(path=calledPath),'a+') # open the .gitignore files
+gitIgnore.write(defaultgitIgnoreFile.read()) # write the .gitignore file with the contents of the default file. 
+
+# close both files
+gitIgnore.close() 
 defaultgitIgnoreFile.close()
 
 os.system("git add README.md") # add README.md
 os.system("git add .gitignore") # add .gitignore
 
-os.system("git commit -m \"initial commit\"")
+os.system("git commit -m \"initial commit\"") # commit both files as the inital commit
 
-# Now lets push everything to github
-folderName = os.path.basename(os.path.normpath(calledPath)) # the folder name is the repo name
+# Moving on to github
+folderName = os.path.basename(os.path.normpath(calledPath)) # get the the folder name to use for the repo name
 
 # Make a repo json
+# You can change this to what your defaults are.
 newRepo = {
   "name": folderName,
   "description": "Made with git-init.py",
@@ -54,22 +55,22 @@ newRepo = {
   "has_wiki": False
 }
 
-# Make github make that repo
+# Tell github to make that repo
 resp = requests.post('https://api.github.com/user/repos', auth=(userName,token), json=newRepo)
-if resp.status_code != 200:
+if resp.status_code != 201: # we expect 201 here. That means "Created"
     # something went wrong ?
-    print(resp.status_code)
+    print(resp.status_code) 
 
-# Get the details of the repo back from github
-resp = requests.get('https://api.github.com/repos/{user}/{repo}'.format(user=userName,repo=folderName), auth=(userName,token))
-
-# Get the ssh_url from that repo
+# The respons from the post request would be the repo details so we get sshURL from that.
 sshURL = resp.json()['ssh_url']
 
-# Add the remote
+# Add the remote ssh URL as origin
 os.system("git remote add origin {remoteURL}".format(remoteURL=sshURL))
 
 # Push to GitHub
 os.system("git push origin master")
 
+# Stop the script here and wait for the user to click exit.
+# This is optional, if you don't like that just comment the line away. 
+# I like to keep it up to see that i did't get any errors along the way. 
 input("Press enter to exit")
